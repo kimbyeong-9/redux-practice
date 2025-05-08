b291feca838423589c0cfc55d616d621607fff00
김형석 <kimhyoungseok@gimhyeongseog-ui-MacBookPro.local>
2025-05-09 00:03:20 +0900

    완료

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import Box from './component/Box'
 function App() {
   const count = useSelector(state => state.count)
   const dispatch = useDispatch();
-
+  const id = useSelector(state => state.id)
+  const password = useSelector(state => state.password)
 
 
 
@@ -17,11 +18,18 @@ function App() {
     dispatch({type: 'INCREMENT', payload:{num: 5}})
     
   }
+
+  const login = () => {
+    dispatch({type: 'LOGIN', payload:{id: 'byeong-9', password: '123'}})
+  }
+
   return (
     <div>
       <h1>{count}</h1>
+      <h1>{password},{id}</h1>
       <button onClick ={increase}>증가!</button>
       <button onClick ={login}>로그인~</button>
+      
       <Box/>
     </div>
   );
